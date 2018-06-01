b0bc519b5e7d2249ca43dde8b4b57d7df0644873
Anar Enhsaihan <aenhsaihan@gmail.com>
2018-06-01 15:43:44 +0800

    Create contracts for transcript request and its factory

---
+++ contracts/TranscriptionRequest.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.18;
+
+contract TranscriptionFactory {
+
+    address owner = msg.sender;
+
+    mapping (address => bool) public verifiedTranscriptionRequests;
+    mapping (address => address[]) public transcriptionRequestsByRequester;
+
+    address[] public deployedTranscriptionRequests;
+
+    event TranscriptRequested(address requester, address request);
+
+    function createTranscriptionRequest() public {
+        address newTranscriptionRequest = new TranscriptionRequest(msg.sender);
+        deployedTranscriptionRequests.push(newTranscriptionRequest);
+        verifiedTranscriptionRequests[newTranscriptionRequest] = true;
+        transcriptionRequestsByRequester[msg.sender].push(newTranscriptionRequest);
+
+        emit TranscriptRequested(msg.sender, newTranscriptionRequest);
+    }
+
+    function verifyTranscriptionRequest(address transcriptionRequest) view public returns (bool) {
+        return verifiedTranscriptionRequests[transcriptionRequest];
+    }
+}
+
+contract TranscriptionRequest {
+
+    address public requester;
+
+    constructor(address _requester) public {
+        requester = _requester;
+    }
+}
