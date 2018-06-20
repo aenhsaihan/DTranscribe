d0c46b96223a5a3c56873815175f439df59366e8
Anar Enhsaihan <aenhsaihan@gmail.com>
2018-06-20 19:20:13 +0700

    Get array of transcription requests from our factory contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,6 +1,10 @@
 import web3 from './web3';
 
-const TranscriptionFactory = artifacts.require('TranscriptionFactory');
+const artifacts = require('../build/contracts/TranscriptionFactory.json');
+const contract = require('truffle-contract');
+
+const TranscriptionFactory = contract(artifacts);
+TranscriptionFactory.setProvider(web3.currentProvider);
 
 const instance = TranscriptionFactory.at(
   '0x6BD6c053595cbA4a15367801cA246c8Ef65be52c'
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
-export default () => <div>Welcome to next.js!</div>;
+import React, { Component } from 'react';
+import factory from '../ethereum/factory';
+
+class TranscriptionIndex extends Component {
+  async componentDidMount() {
+    const transcriptionRequests = await factory.getDeployedTranscriptionRequests.call();
+
+    console.log(transcriptionRequests);
+  }
+
+  render() {
+    return <div>Transcription Index!</div>;
+  }
+}
+
+export default TranscriptionIndex;
