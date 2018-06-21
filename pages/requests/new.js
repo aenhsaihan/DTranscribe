ba4b9ad88c2d86960c670661dab73401c5acf629
Anar Enhsaihan <aenhsaihan@gmail.com>
2018-06-22 02:21:40 +0700

    Create forms to be used when creating a transcription request

--- pages/requests/new.js
+++ pages/requests/new.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import { Form, Button, Input } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 
 class TranscriptionRequestNew extends Component {
   render() {
     return (
       <Layout>
-        <h1>New Transcription Request!</h1>
+        <h3>Create a Transcription Request</h3>
+
+        <Form>
+          <Form.Field>
+            <label>Request IPFS Hash</label>
+            <Input
+              label="https://ipfs.io/ipfs/"
+              placeholder="QmfWCE442XEYHoSWRTVtjKjNAsEDkDm4EF9zuTrgVmhZ9i"
+            />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Duration of transcription phase</label>
+            <input />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Duration of voting</label>
+            <input />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Target language</label>
+            <input />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Target accent</label>
+            <input />
+          </Form.Field>
+        </Form>
+
+        <Button primary>Create!</Button>
       </Layout>
     );
   }
