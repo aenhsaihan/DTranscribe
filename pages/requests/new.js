c984e422d701d3110e9265a40e8507b979d5a012
Anar Enhsaihan <aenhsaihan@gmail.com>
2018-06-22 01:47:54 +0700

    Create nested route for new transcription requests

---
+++ pages/requests/new.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class TranscriptionRequestNew extends Component {
+  render() {
+    return <h1>New Transcription Request!</h1>;
+  }
+}
+
+export default TranscriptionRequestNew;
