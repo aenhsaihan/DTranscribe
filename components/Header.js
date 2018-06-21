6e863267fc9ff3163fe2e676611692dc47429041
Anar Enhsaihan <aenhsaihan@gmail.com>
2018-06-22 00:37:27 +0700

    Use styling in the menu header to add some margin at the top

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import { Menu } from 'semantic-ui-react';
 
 export default () => {
   return (
-    <Menu>
+    <Menu style={{ marginTop: '10px' }}>
       <Menu.Item>DTranscribe</Menu.Item>
       <Menu.Menu position="right">
         <Menu.Item>Requests</Menu.Item>
