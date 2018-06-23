dd8a8bcb316fe3b47f2f794185e5c60112794366
Anar Enhsaihan <aenhsaihan@gmail.com>
2018-06-23 23:23:05 +0700

    Render Layout for page which displays information for a specific transcription request

--- pages/requests/show.js
+++ pages/requests/show.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import Layout from '../../components/Layout';
 
 class RequestShow extends Component {
   render() {
-    return <h3>Request Show</h3>;
+    return (
+      <Layout>
+        <h3>Request Show</h3>
+      </Layout>
+    );
   }
 }
 
