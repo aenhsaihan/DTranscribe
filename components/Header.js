2ce2d40836f0171e6e0f716867ca984dd3f477f3
Anar Enhsaihan <aenhsaihan@gmail.com>
2018-06-23 22:48:56 +0700

    Create a link route to help user navigate back to the main page

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 export default () => {
   return (
     <Menu style={{ marginTop: '10px' }}>
-      <Menu.Item>DTranscribe</Menu.Item>
+      <Link route="/">
+        <a className="item">DTranscribe</a>
+      </Link>
+
       <Menu.Menu position="right">
         <Menu.Item>Requests</Menu.Item>
 
