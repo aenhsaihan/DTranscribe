778c77d5e862fea9a042db88f83ffc5b8554844f
Anar Enhsaihan <aenhsaihan@gmail.com>
2018-06-25 04:47:28 +0700

    Pass index of transcription to vote function on contract

--- components/TranscriptionRow.js
+++ components/TranscriptionRow.js
@@ -7,10 +7,11 @@ class TranscriptionRow extends Component {
   onVote = async () => {
     const transcriptionRequest = TranscriptionRequest(this.props.address);
 
+    const transcriberId = this.props.id;
     const transcriber = this.props.transcription[3];
 
     const [account] = await web3.eth.getAccounts();
-    await transcriptionRequest.voteForTranscriber(transcriber, {
+    await transcriptionRequest.voteForTranscriber(transcriber, transcriberId, {
       from: account
     });
   };
