25165691df6c9c8015c4067d05ce7f0b8af9e4ce
Anar Enhsaihan <aenhsaihan@gmail.com>
2018-06-24 08:56:28 +0700

    get the count of all transcriptions under a request and get each transcription details one by one using fancy javascript

--- pages/requests/transcriptions/index.js
+++ pages/requests/transcriptions/index.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
+import Layout from '../../../components/Layout';
+import TranscriptionRequest from '../../../ethereum/transcriptionRequest';
 
 class TranscriptionIndex extends Component {
+  static async getInitialProps(props) {
+    const { address } = props.query;
+    const transcriptionRequest = TranscriptionRequest(address);
+    const transcriptionsCount = await transcriptionRequest.getTranscriptionsCount.call();
+
+    const transcriptions = await Promise.all(
+      Array(transcriptionsCount.toNumber())
+        .fill()
+        .map((element, index) => {
+          return transcriptionRequest.transcriptions.call(index);
+        })
+    );
+
+    return { address };
+  }
   render() {
-    return <h3>Transcription List</h3>;
+    return (
+      <Layout>
+        <h3>Transcriptions</h3>
+        <Link>
+          <a>
+            <Button primary>Add Request</Button>
+          </a>
+        </Link>
+      </Layout>
+    );
   }
 }
 
