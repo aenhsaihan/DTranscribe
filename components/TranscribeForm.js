5b37296f67dbe3742454435bca48b62927f5abb0
Anar Enhsaihan <aenhsaihan@gmail.com>
2018-06-26 10:14:14 +0700

    allow transcriber to upload transcription file to ipfs

--- components/TranscribeForm.js
+++ components/TranscribeForm.js
@@ -1,38 +1,52 @@
 import React, { Component } from 'react';
 import { Button, Form, Input, Message } from 'semantic-ui-react';
+import InputIPFS from '../components/InputIPFS';
 import TranscriptionRequest from '../ethereum/transcriptionRequest';
 import web3 from '../ethereum/web3';
+import ipfs from '../ethereum/ipfs';
 import { Router } from '../routes';
 
 class TranscribeForm extends Component {
   state = {
-    transcriptionIPFSHash: '',
     errorMessage: '',
-    loading: false
+    loading: false,
+    ipfsHash: '',
+    buffer: ''
   };
 
-  onSubmit = async event => {
-    event.preventDefault();
+  saveBuffer = buffer => {
+    this.setState({ buffer });
+  };
 
+  submitTransaction = async () => {
     const transcriptionRequest = TranscriptionRequest(this.props.address);
-
-    this.setState({ loading: true, errorMessage: '' });
 
     try {
       const accounts = await web3.eth.getAccounts();
-      await transcriptionRequest.transcribeRequest(
-        this.state.transcriptionIPFSHash,
-        {
-          from: accounts[0]
-        }
-      );
+      await transcriptionRequest.transcribeRequest(this.state.ipfsHash, {
+        from: accounts[0]
+      });
 
       Router.replaceRoute(`/requests/${this.props.address}`);
     } catch (err) {
       this.setState({ errorMessage: err.message });
     } finally {
-      this.setState({ loading: false, transcriptionIPFSHash: '' });
+      this.setState({ loading: false });
     }
+  };
+
+  onSubmit = async event => {
+    event.preventDefault();
+
+    this.setState({ loading: true, errorMessage: '' });
+
+    // save document to IPFS, return its hash#, and set hash# to state
+    await ipfs.add(this.state.buffer, (err, ipfsHash) => {
+      console.log(err, ipfsHash);
+      this.setState({ ipfsHash: ipfsHash[0].hash });
+
+      this.submitTransaction();
+    });
   };
 
   render() {
@@ -40,14 +54,7 @@ class TranscribeForm extends Component {
       <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Submit Transcription IPFS Hash here</label>
-          <Input
-            value={this.state.transcriptionIPFSHash}
-            onChange={event =>
-              this.setState({ transcriptionIPFSHash: event.target.value })
-            }
-            label="https://ipfs.io/ipfs/"
-            placeholder="QmfWCE442XEYHoSWRTVtjKjNAsEDkDm4EF9zuTrgVmhZ9i"
-          />
+          <InputIPFS saveBuffer={this.saveBuffer} />
         </Form.Field>
 
         <Message error header="Oops!" content={this.state.errorMessage} />
