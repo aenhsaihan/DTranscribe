878e6cef76d51595d672574c02206a9db113b6ea
Anar Enhsaihan <aenhsaihan@gmail.com>
2018-07-09 10:27:30 +0300

    Add support for different languages

--- common.js
+++ common.js
@@ -1,5 +1,11 @@
 const countryOptions = [
-  { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' }
+  { key: 'gb', value: 'gb', flag: 'gb', text: 'English' },
+  { key: 'es', value: 'es', flag: 'es', text: 'Spanish' },
+  { key: 'mn', value: 'mn', flag: 'mn', text: 'Mongolian' },
+  { key: 'ru', value: 'ru', flag: 'ru', text: 'Russian' },
+  { key: 'bg', value: 'bg', flag: 'bg', text: 'Bulgarian' },
+  { key: 'jp', value: 'jp', flag: 'jp', text: 'Japanese' },
+  { key: 'th', value: 'th', flag: 'th', text: 'Thailand' }
 ];
 
 export default countryOptions;
